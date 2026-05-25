b73b132edb4534e860dd1c8f8d82fe91a262be36
JanviPrajapati0210 <24aiml052@charusat.edu.in>
2026-05-25 11:29:16 +0530

    initial project setup with Flask app and config

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(
-    __name__,
-    template_folder="frontend/templates",
-    static_folder="frontend/static"
-)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
